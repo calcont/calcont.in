d35054e46df107a77c86f95db981fbef5769dd88
Amar <amarkhamka\r6@gmail.com>
2021-05-22 19:11:47 +0530

    redirecintg

--- basicsite/settings.py
+++ basicsite/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'mysite.apps.MysiteConfig',
     
     'django.contrib.sitemaps',
-    'django.contrib.sites',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
