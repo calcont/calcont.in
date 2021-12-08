c9ea8ea102ab8519b7a3e49f110d1ad40a1293bc
Amar K <amarkhamkar6@gmail.com>
2021-12-08 14:09:21 +0530

    contact spam checker-6 error debug

--- mysite/views.py
+++ mysite/views.py
@@ -439,9 +439,10 @@ def ContactMe(request):
     if request.method == "POST":
         name=request.POST.get("name")
         email=request.POST.get("email")
+   
         desc=request.POST.get("desc")
-        PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
         import joblib
+        PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
         models = joblib.load(f'./staticfiles/spam.pkl')
         v = joblib.load(f'./staticfiles/vector.pickel')
         spam = models.predict(v.transform([desc]))
@@ -453,6 +454,7 @@ def ContactMe(request):
         contact.save()
         than=True
         return render(request,'Contact_me.html',{"than":than})
+    return render(request,'Contact_me.html')
 def Aboutme(request):
     return render(request,'Aboutme.html')
 def PrivacyPolicy(request):
