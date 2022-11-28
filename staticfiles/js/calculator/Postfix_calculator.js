bca21db83b9d47e1551223304c3d8673338e71fe
Amark19 <amarkhamkar6@gmail.com>
2022-11-28 13:54:56 +0530

    :p fixed

---
+++ staticfiles/js/calculator/Postfix_calculator.js
@@ -0,0 +1,40 @@
+$(".cal").click(function (e) {
+    var postfix = document.getElementById("Postfix").value;
+    var stack = [],
+      top = -1;
+  
+    var i, a, b, result, pEval, ch;
+  
+    for (i = 0; i <= postfix.length; i++) {
+      ch = postfix[i];
+  
+      if (!isNaN(parseInt(ch))) {
+        stack[++top] = parseInt(ch);
+      } else if (ch == "+" || ch == "-" || ch == "*" || ch == "/") {
+        b = stack[top];
+        top--;
+        a = stack[top];
+        top--;
+  
+        if (ch == "+") {
+          result = a + b;
+        } else if (ch == "-") {
+          result = a - b;
+        } else if (ch == "*") {
+          result = a * b;
+        } else if (ch == "/") {
+          result = a / b;
+        }
+        stack[++top] = result;
+      }
+    }
+  
+    pEval = stack[top];
+  
+    // top--;
+  
+    document.getElementById("Ans").value = pEval;
+  
+    e.preventDefault();
+  });
+  
