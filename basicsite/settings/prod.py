cc4da7f6f944b1c5cc7218d3e42c1ae95c08a927
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2023-01-18 23:13:10 +0530

    debug changes to true for verifying error in prod

--- basicsite/settings/prod.py
+++ basicsite/settings/prod.py
@@ -9,7 +9,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.calcont.in', 'calcont.in']
 
