09dbac99c86241728c45f49718961c60de95a15a
Amark19 <amarkhamkar6@gmail.com>
2023-01-22 12:55:55 +0530

    added env in github actions done changes accordingly in testing.yml

--- basicsite/settings/dev.py
+++ basicsite/settings/dev.py
@@ -1,7 +1,11 @@
 from basicsite.settings.base import *
 import os
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-SECRET_KEY = "b2317f5cd3d1290d1c0eb3e0d2a2235905a3244e487ec3d57e" # dummy secret key(not og)
+SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = True
 
@@ -12,5 +16,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles")
 ]
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '475883331625-q7vvbr7ovpjs7lhjr0rfdoil53gd1r69.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '475883331625-q7vvbr7ovpjs7lhjr0rfdoil53gd1r69.apps.googleusercontent.com'# for development purpose
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'y8FZoB0bV4DceEm6pFg1mmpe'
