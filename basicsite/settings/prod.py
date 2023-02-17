d16a64ca4618f9f6ade4560e8ee4ec5517946455
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2023-02-18 00:06:29 +0530

    Update prod.py

--- basicsite/settings/prod.py
+++ basicsite/settings/prod.py
@@ -2,6 +2,7 @@ from basicsite.settings.base import *
 import os
 import django_heroku
 import environ
+import dj_database_url
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
@@ -9,7 +10,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = False
+DEBUG = env('DEBUG')
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = ['www.calcont.in', 'calcont.in' ,'www.calcont.herokuapp.com']
 
