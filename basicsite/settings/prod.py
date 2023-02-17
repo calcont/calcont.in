614485e67c0ac16d421f7216e787d0595656a687
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2023-02-17 23:36:58 +0530

    Update prod.py

--- basicsite/settings/prod.py
+++ basicsite/settings/prod.py
@@ -2,7 +2,6 @@ from basicsite.settings.base import *
 import os
 import django_heroku
 import environ
-import dj_database_url
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
@@ -13,9 +12,6 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['www.calcont.in', 'calcont.in' ,'www.calcont.herokuapp.com']
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
