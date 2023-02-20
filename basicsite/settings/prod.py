fd6c77a148a37f3190cff0d8a124352253ba1600
Amark19 <amarkhamkar6@gmail.com>
2023-02-20 21:14:09 +0530

    automated admin email report handlin

--- basicsite/settings/prod.py
+++ basicsite/settings/prod.py
@@ -10,6 +10,16 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
+ADMINS = [
+    ('CalConT', 'calcont.in01@gmail.com'),
+]
+
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST_USER = ' postmaster@sandbox0c4d0d0425594b65865fb83a4b57c093.mailgun.org'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
 DEBUG = False
 
 db_from_env = dj_database_url.config(conn_max_age=500)
