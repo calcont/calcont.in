d1c7ea50bdd2888066735bb7e51117944937529f
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2024-03-05 23:33:59 +0530

    stage support (#80)
    * stage support
    
    * removed ssl-redirect

---
+++ basicsite/settings/stage.py
@@ -0,0 +1,37 @@
+from basicsite.settings.base import *
+import os
+import django_heroku
+import environ
+import dj_database_url
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = False
+
+ADMINS = [
+    ('CalConT', 'calcont.in01@gmail.com'),
+]
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = ['.calcont.in', 'calcont.in', '.herokuapp.com']
+
+SECURE_HSTS_SECONDS = 60
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+django_heroku.settings(locals())
+
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
