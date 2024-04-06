1cf0a530f733bef126a736a87a149159f1e6b3fc
Amark19 <amarkhamkar6@gmail.com>
2024-04-06 16:07:57 +0530

    formatted files

--- mysite/handlers/requestHandler/post.py
+++ mysite/handlers/requestHandler/post.py
@@ -1,7 +1,8 @@
 from . import base
 import requests
 
-class PostHandler(base.Request):    
+
+class PostHandler(base.Request):
 
     def send(self):
         response = requests.post(self.url, data=self.data, headers=self.headers)
