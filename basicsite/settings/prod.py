ebf7f15d916020643a7b7074d105fdac0c7c108e
Amark19 <amarkhamkar6@gmail.com>
2024-04-14 23:51:57 +0530

    Added .com support

--- basicsite/settings/prod.py
+++ basicsite/settings/prod.py
@@ -25,7 +25,7 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = ['.calcont.in', 'calcont.in', '.herokuapp.com']
+ALLOWED_HOSTS = ['.calcont.in', 'calcont.in', '.herokuapp.com', '.calcont.com', 'calcont.com']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
