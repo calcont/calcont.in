96cb011c5bd61a79c1a5cb2a33247832098a3e23
Amark19 <amarkhamkar6@gmail.com>
2025-04-20 21:55:20 +0530

    Update prod db key

--- basicsite/settings/prod.py
+++ basicsite/settings/prod.py
@@ -3,9 +3,12 @@ import os
 import django_heroku
 import environ
 import dj_database_url
+
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+DB_KEY = 'EXTERNAL_DATABASE_URL'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
@@ -22,7 +25,7 @@ EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(env=DB_KEY, conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = ['.calcont.in', 'calcont.in', '.herokuapp.com', '.calcont.com', 'calcont.com']
@@ -41,7 +44,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 django_heroku.settings(locals())
-
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
