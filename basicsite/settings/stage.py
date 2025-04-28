484a8886a091c88daa2fdf1c8b17e8afac5839bd
Amark19 <amarkhamkar6@gmail.com>
2025-04-28 23:36:05 +0530

    handling static files

--- basicsite/settings/stage.py
+++ basicsite/settings/stage.py
@@ -26,7 +26,10 @@ SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
