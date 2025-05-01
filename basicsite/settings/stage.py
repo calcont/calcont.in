a65e964485cb803095d27f7dcaff7551525b2596
Amark19 <amarkhamkar6@gmail.com>
2025-05-01 19:26:23 +0530

    GET DEBUG FROM ENV in stage

--- basicsite/settings/stage.py
+++ basicsite/settings/stage.py
@@ -9,7 +9,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = False
+DEBUG = env('DEBUG')
 
 ADMINS = [
     ('CalConT', 'calcont.in01@gmail.com'),
@@ -27,7 +27,7 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 CSRF_TRUSTED_ORIGINS = [
-    '.railway.app',
+    'https://web-staging-4e14.up.railway.app/',
 ]
 
 
