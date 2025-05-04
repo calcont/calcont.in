a2398a24047079dc6bb6facdd68078359c41d9d1
Amark19 <amarkhamkar6@gmail.com>
2025-05-04 21:16:34 +0530

    Added logging again in currency converter

--- mysite/all_views/views_converters.py
+++ mysite/all_views/views_converters.py
@@ -4,6 +4,7 @@ import json
 import base64
 import requests
 import random
+import logging
 from django.core.cache import cache
 from .. import MyFunctions
 from ..constants import converters as constants
@@ -11,6 +12,7 @@ from ..handlers.requestHandler.get import GetHandler
 
 SideMap = MyFunctions.ArrangeSideMapForWebpage()
 CACHE_TIMEOUT = 60 * 60 * 24
+logger = logging.getLogger(__name__)
 
 
 def generate_currency_endpoint(from_currency):
@@ -50,7 +52,9 @@ def Currencyconversion(request):
             get_request = GetHandler(endpoint)
             response = get_request.send()
             cache.set(from_currency, response, CACHE_TIMEOUT)
+            logger.info(f"Cache miss for {from_currency}. Data fetched from API.")
         data = cache.get(from_currency).json()
+        logger.info(f"Cache hit for {from_currency}. Data fetched from cache.")
         conversion_rate = data['data'][to_currency]['value']
         converted_amount = float(amount) * conversion_rate
         json_response = json.dumps({'converted_amount': converted_amount}, default=str)
