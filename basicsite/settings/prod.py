8305d284fafa8cd9453a34b079c80be0edc913e3
Amark19 <amarkhamkar6@gmail.com>
2025-11-02 02:04:04 +0530

    added django email_backend

--- basicsite/settings/prod.py
+++ basicsite/settings/prod.py
@@ -18,6 +18,8 @@ ADMINS = [
     ('CalConT', 'calcont.in01@gmail.com'),
 ]
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
